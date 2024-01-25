fc114cf178dfa3c0d3f915b5f0efb23ab2219659
EmmaDenton <edenton34@gmail.com>
2024-01-25 21:15:42 +1100

    Deleting routes folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,26 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 import Map from './components/Map';
+import SearchPolicy from './pages/SearchPolicy';
 
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
+const handlePageChange = (page) => setCurrentPage(page);
+
 
 function App() {
+  const renderPage = () => {
+    switch (currentPage) {
+      case '/':
+        return <SearchPolicy />;
+      }}
   return (
     <ApolloProvider client={client}>
     <>
-      <Navbar />
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
       <Outlet />
       <Map />
     </>
