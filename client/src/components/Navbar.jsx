ec3421050324f206db346f954b2ba5b30b86df96
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-30 21:03:44 +1100

    navbar style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-
+import logo from '../assets/img/global.png';
 const Navbar = () => {
   const currentPage = useLocation().pathname;
   return (
-    <div class="ui fixed inverted menu" >
+    <div class="ui orange fixed inverted menu" >
       <div class="ui container">
         <a class="header item">
           <Link
             to="/"
-            className={currentPage === '/Home' ? 'nav-link active' : 'nav-link'}
-            >Global Policy Tracker
+            className={currentPage === '/Home' ? 'nav-link active' : 'nav-link'} 
+            ><img src={logo} alt="logo" class='logo' />
             </Link>
         </a>
 
         <div class="right menu">
-          <a>
+          <a class="item" id='donateLink'>
             <Link
             to="/donate"
-            className={currentPage === '/Donate' ? 'nav-link active' : 'nav-link'} class="item"
+            className={currentPage === '/Donate' ? 'nav-link active' : 'nav-link'} class="item" id='itemDonate'
             >
             Donate
             </Link>
           </a>
           
           <div>
-            <a >
+            <a class="item" id='loginLink'>
             <Link
             to="/login"
-            className={currentPage === '/LoginForm' ? 'nav-link active' : 'nav-link'} class="item"
+            className={currentPage === '/LoginForm' ? 'nav-link active' : 'nav-link'} class="item" id='itemLogin'
             >
-            Login
+            Log in
             </Link>
             </a>
           </div>
